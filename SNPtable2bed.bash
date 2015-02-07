251742ff6460752fa8909988803227327e778331
Gregory Owens <gregory.owens@alumni.ubc.ca>
2015-02-06 16:30:36 -0800

    New snp to bed formatter

---
+++ SNPtable2bed.bash
@@ -0,0 +1,18 @@
+#This file converts .tab file to .bed
+
+
+reformat=/home/owens/bin/reformat
+tassel=/home/owens/bin/tassel3-standalone
+plink=/home/owens/bin/plink-1.07-x86_64
+input=$1
+name=${input%%.tab}
+echo "Converting ${name}.tab to ${name}.plk.bed"
+perl $reformat/SNPtable2hmp.pl ${name}.tab > ${name}.hmp
+
+$tassel/run_pipeline.pl -fork1 -h ${name}.hmp -export $name -exportType Plink -runfork1
+
+$plink/plink --file ${name}.plk --out ${name}.plk --make-bed --noweb
+
+rm ${name}.hmp
+rm ${name}.plk.map
+rm ${name}.plk.ped
